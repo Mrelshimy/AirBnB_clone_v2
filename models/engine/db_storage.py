e16d1b24def085b3646194aa4b8dd46bc1cf44e7
nono <nour98nour@gmail.com>
2024-02-19 00:23:20 +0200

    edited db again

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,7 +3,7 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-from models.base_model import BaseModel, Base
+from models.base_model import Base
 from models.user import User
 from models.place import Place
 from models.state import State
