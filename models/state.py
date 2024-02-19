5537856ef8b3495c869b95179b614c9204725a8a
Mrelshimy <mraafat.elsayed@gmail.com>
2024-02-19 09:06:33 +0200

    Update state

--- models/state.py
+++ models/state.py
@@ -12,17 +12,17 @@ class State(BaseModel, Base):
 
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade='all, delete')
+        cities = relationship('City', backref='state', cascade='all, delete-orphan')
     else:
         name = ''
 
-    @property
-    def cities(self):
-        """getter funtion to get cities of certain state"""
-        from models import storage
-        city_dict = storage.all("City")
-        state_cities = []
-        for value in city_dict.values():
-            if value.state_id == self.id:
-                state_cities.append(value)
-        return state_cities
+        @property
+        def cities(self):
+            """getter funtion to get cities of certain state"""
+            from models import storage
+            city_dict = storage.all("City")
+            state_cities = []
+            for value in city_dict.values():
+                if value.state_id == self.id:
+                    state_cities.append(value)
+            return state_cities
