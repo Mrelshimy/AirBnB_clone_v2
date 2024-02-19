4d00f39e40111366fd58effff20463ba7128d42d
Mrelshimy <mraafat.elsayed@gmail.com>
2024-02-19 09:09:52 +0200

    updat city

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column, ForeignKey
+from sqlalchemy.orm import relationship
 import os
 
 
@@ -12,6 +13,7 @@ class City(BaseModel, Base):
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        state = relationship('State', back_populates='cities')
     else:
         state_id = ""
         name = ""
