6ed92d5b685c257d49495c907148265e9cd73e37
nono <nour98nour@gmail.com>
2024-02-20 14:31:28 +0200

    fixed bug id or created_at or updated_at not in kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -32,6 +32,12 @@ class BaseModel:
                     continue
                 else:
                     setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
