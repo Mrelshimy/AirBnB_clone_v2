b1ef6633d03ae7523ee4fb3667c194c6b2fdf3ba
nono <nour98nour@gmail.com>
2024-02-21 07:54:38 +0200

    fixed betty for test_db

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -4,8 +4,9 @@ import unittest
 from models.engine.db_storage import DBStorage
 import os
 
-@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', "this test just for db")
+
+@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
+                 "this test just for db")
 class test_dbStorage(unittest.TestCase):
     def test_documentation(self):
         self.assertIsNotNone(DBStorage.__doc__)
-    
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,7 +6,8 @@ from models import storage
 import os
 
 
-@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db', "this test just for FILE storage")
+@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db',
+                 "this test just for Fs")
 class test_fileStorage(unittest.TestCase):
     """ Class to test the file storage method """
 
@@ -22,7 +23,7 @@ class test_fileStorage(unittest.TestCase):
         """ Remove storage file at end of tests """
         try:
             os.remove('file.json')
-        except:
+        finally:
             pass
 
     def test_obj_list_empty(self):
