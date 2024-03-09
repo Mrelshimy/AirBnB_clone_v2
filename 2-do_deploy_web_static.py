05151e599a71c8ea9f0c20d6f71991b8a0e3b135
Mrelshimy <mraafat.elsayed@gmail.com>
2024-03-09 17:14:38 +0200

    adding files

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Fab file to deploy web_static archive to servers"""
+
+from fabric.api import *
+import os
+
+env.hosts = ['52.3.255.212', '100.24.235.178']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/school'
+
+
+def do_deploy(archive_path):
+    """Function to deploy archive to servers"""
+    if not os.path.exists(archive_path):
+        return False
+    name = archive_path[9:-4]
+    archive_name = archive_path[9:]
+    try:
+        put(archive_path, '/tmp/')
+        run(f'sudo mkdir -p /data/web_static/releases/{name}/')
+        run(f'sudo tar -xzf /tmp/{archive_name} -C\
+ /data/web_static/releases/{name}/')
+        run(f'sudo rm /tmp/{archive_name}')
+        run(f'sudo mv /data/web_static/releases/{name}/web_static/*\
+ /data/web_static/releases/{name}/')
+        run(f'sudo rm -r /data/web_static/releases/{name}/web_static')
+        run(f'sudo rm -rf /data/web_static/current')
+        run(f'sudo ln -s /data/web_static/releases/{name}\
+ /data/web_static/current')
+    except Exception:
+        return False
+    return True
