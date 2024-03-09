8763a92bb131b385b0b3a61fa49fc0d73070c171
Mrelshimy <mraafat.elsayed@gmail.com>
2024-03-09 05:34:30 -0800

    update

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,19 +4,17 @@ from the contents of the web_static folder """
 
 from fabric.api import local
 import datetime
-import os
 
 
 def do_pack():
     """Function to add files to archive"""
 
-    create_folder = local("mkdir -p versions")
-    if create_folder.stderr != "":
-        return None
-    d_now = datetime.datetime.utcnow()
-    file = f"web_static_{d_now.year}{d_now.month}\
+    try:
+        create_folder = local("mkdir -p versions")
+        d_now = datetime.datetime.utcnow()
+        file = f"web_static_{d_now.year}{d_now.month}\
 {d_now.day}{d_now.hour}{d_now.minute}{d_now.second}"
-    archive = local(f"tar -cvzf versions/{file} web_static")
-    if archive.stderr != "":
+        archive = local(f"tar -cvzf versions/{file} web_static")
+        return f"versions/web_static_{file}.tgz"
+    except Exception:
         return None
-    return f"versions/web_static_{file}.tgz"
