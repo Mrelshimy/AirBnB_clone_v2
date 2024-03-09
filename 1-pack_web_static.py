6e26f2d5af05ae0a08d757cc7095c26196ddf820
Mrelshimy <mraafat.elsayed@gmail.com>
2024-03-09 15:44:54 +0200

    update

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -14,7 +14,7 @@ def do_pack():
         d_now = datetime.datetime.utcnow()
         file = f"web_static_{d_now.year}{d_now.month}\
 {d_now.day}{d_now.hour}{d_now.minute}{d_now.second}"
-        archive = local(f"tar -cvzf versions/{file} web_static")
+        archive = local(f"tar -cvzf versions/{file}.tgz web_static")
         return f"versions/web_static_{file}.tgz"
     except Exception:
         return None
