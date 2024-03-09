8c74c43d9de0d4a1325e1b816c8e06c1c54cbe36
Mrelshimy <mraafat.elsayed@gmail.com>
2024-03-09 05:27:56 -0800

    update

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Fab file to generates a .tgz archive
+from the contents of the web_static folder """
+
+from fabric.api import local
+import datetime
+import os
+
+
+def do_pack():
+    """Function to add files to archive"""
+
+    create_folder = local("mkdir -p versions")
+    if create_folder.stderr != "":
+        return None
+    d_now = datetime.datetime.utcnow()
+    file = f"web_static_{d_now.year}{d_now.month}\
+{d_now.day}{d_now.hour}{d_now.minute}{d_now.second}"
+    archive = local(f"tar -cvzf versions/{file} web_static")
+    if archive.stderr != "":
+        return None
+    return file.os.path()
