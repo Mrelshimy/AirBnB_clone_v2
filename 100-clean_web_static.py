3e59ce2f397c75d7c0f8d25e2febafc0109a87f0
Mohamed Raafat <138755670+Mrelshimy@users.noreply.github.com>
2024-03-10 13:40:05 +0200

    update

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -2,28 +2,20 @@
 import os
 from fabric.api import *
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['52.3.255.212', '100.24.235.178']
 
 
 def do_clean(number=0):
-    """Delete out-of-date archives.
-
-    Args:
-        number (int): The number of archives to keep.
-
-    If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives,
-    etc.
-    """
+    """Delete out-of-date archives"""
     number = 1 if int(number) == 0 else int(number)
 
     archives = sorted(os.listdir("versions"))
     [archives.pop() for i in range(number)]
     with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+        [local("rm ./{}".format(x)) for x in archives]
 
     with cd("/data/web_static/releases"):
         archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
+        archives = [x for x in archives if "web_static_" in x]
         [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+        [run("rm -rf ./{}".format(x)) for x in archives]
