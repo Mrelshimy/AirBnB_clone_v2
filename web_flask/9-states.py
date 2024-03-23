32ead4d0ad8688b0de4cdbe8768ee96d7e07d881
Mrelshimy <mraafat.elsayed@gmail.com>
2024-03-23 23:41:00 +0200

    update

---
+++ web_flask/9-states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Flask web_app module"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+web_app = Flask(__name__)
+
+
+@web_app.route("/states", strict_slashes=False)
+def show_states():
+    """ Function to return list of states"""
+    states = storage.all(State)
+    return render_template("7-states_list.html",
+                           states=states)
+
+
+@web_app.route("/states/<id>", strict_slashes=False)
+def show_state_cities(id):
+    """ Function to return list of states"""
+    states = storage.all(State)
+    return render_template("9-states.html",
+                           states=states, id=id)
+
+
+@web_app.teardown_appcontext
+def teardown(self):
+    """Funtion to remove current SQLalchemy session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    web_app.run()
