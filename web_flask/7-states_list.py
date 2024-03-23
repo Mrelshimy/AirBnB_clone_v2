79779d6748b438998305d79195a7c0154c189ad6
Mrelshimy <mraafat.elsayed@gmail.com>
2024-03-23 13:53:08 +0200

    update

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Flask web_app module"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+web_app = Flask(__name__)
+
+
+@web_app.route("/state_list", strict_slashes=False)
+def show_states():
+    """ Function to return list of states"""
+    states = storage.all(states)
+    return render_template("7-states_list.html",
+                                   states=states)
+
+@web_app.teardown_appcontext
+def session_close(self):
+    """Funtion to remove current SQLalchemy session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    web_app.run()
